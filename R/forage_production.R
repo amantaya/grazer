b03f7c171a22a122f9cc637fb10ac97c93e81409
amantaya <aantaya@email.arizona.edu>
2025-03-05 14:36:29 -0700

    docs: update documentation for calc_plot_area and calc_forage_prod functions to improve clarity and detail

--- R/forage_production.R
+++ R/forage_production.R
@@ -1,10 +1,13 @@
-#' Title
+#' Calculate the Area of a Sampling Frame
 #'
-#' @param x
+#' @param dim_x The length of the quadrat in the x-dimension
+#' @param dim_y The length of the quadrat in the y-dimension
+#' @param units The units of the quadrat dimensions
 #'
-#' @return
+#' @return The area of the sampling frame in the units provided.
 #'
 #' @examples
+#' calc_plot_area(dim_x = 30, dim_y = 30, units = "cm")
 #'
 #' @export
 #' @importFrom units set_units
@@ -34,13 +37,18 @@ calc_plot_area <- function(dim_x, dim_y, units) {
     return(sample_area)
 }
 
-#' Title
+#' Calculate Forage Production
 #'
-#' @param x
+#' @param sample_wt The weight of the forage sample.
+#' @param sample_units The units of the forage sample weight.
+#' @param sample_area The area of the sampling frame. Most likely calculated using \code{\link{calc_plot_area}}.
+#' @param output_units The units to return the forage production in. Either 'kg/ha' or 'lb/acre'.
 #'
-#' @return
+#' @return The forage production in the units provided, either 'kg/ha' or 'lb/acre'.
 #'
 #' @examples
+#' sample_area <- calc_plot_area(dim_x = 50, dim_y = 50, units = "cm")
+#' calc_forage_prod(sample_wt = 100, sample_units = "g", sample_area = sample_area, output_units = "kg/ha")
 #'
 #' @export
 #' @importFrom units set_units
@@ -51,7 +59,7 @@ calc_forage_prod <- function(sample_wt, sample_units, sample_area, output_units)
     sample_area_m2 <- units::set_units(sample_area, "m^2")
 
     sample_wt_per_m2 <- sample_wt/sample_area_m2
-
+    
     if (output_units == "kg/ha") {
         forage_production <- units::set_units(sample_wt_per_m2, "kg/hectare")
     } else if (output_units == "lb/acre") {
@@ -59,7 +67,5 @@ calc_forage_prod <- function(sample_wt, sample_units, sample_area, output_units)
     } else {
         stop("output_units must be either 'kg/ha' or 'lb/acre'.")
     }
-
     return(forage_production)
-
 }
