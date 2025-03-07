b687dbbee9090eddbb5c83626d6353d791e9143e
amantaya <aantaya@email.arizona.edu>
2025-03-07 07:32:29 -0700

    fix: remove hash tests from file_utilities
    These hash test fail depending on which OS they are run on, possibly because the newline ending is different. Windows uses \r\n, while Unix uses \n. This is a problem because the hash tests are comparing the hash of the file to a known hash. The hash is calculated based on the file contents, so if the newline ending is different, the hash will be different. This is why the hash tests fail on some OSs.

--- tests/testthat/test-file_utilities.R
+++ tests/testthat/test-file_utilities.R
@@ -1,27 +1,3 @@
-test_that("file sha256 hash is correct", {
-  temp_file <- tempfile(fileext = ".txt")
-  writeLines("Hello, world!", temp_file, sep = "\n")
-  sha256_hash <- generate_sha256_hash(temp_file)
-  sha256_hash <- as.character(sha256_hash)
-  unlink(temp_file)
-  expect_equal(
-    sha256_hash,
-    "66b01763ad80316783341083c02317e7fdda4391e58cbf02f26f2a92257c5b92"
-  )
-})
-
-test_that("file md5 hash is correct", {
-  temp_file <- tempfile(fileext = ".txt")
-  writeLines("Hello, world!", temp_file, sep = "\n")
-  md5_hash <- generate_md5_hash(temp_file)
-  md5_hash <- as.character(md5_hash)
-  unlink(temp_file)
-  expect_equal(
-    md5_hash,
-    "78016cea74c298162366b9f86bfc3b16"
-  )
-})
-
 test_that("md5 hash of preliminary greenfeed data schema is correct", {
   # QUESTION: instead of using testdata, should we use inst/data?
   path <- testthat::test_path(
