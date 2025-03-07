23884219a4d9e50cff93115d9e650f8e645c0858
amantaya <aantaya@email.arizona.edu>
2025-03-07 14:22:50 -0700

    test: remove commented-out tests for strict_read_csv function

--- tests/testthat/test-file_utilities.R
+++ tests/testthat/test-file_utilities.R
@@ -24,30 +24,6 @@ test_that("sha256 hash of preliminary greenfeed data schema is correct", {
   )
 })
 
-# test_that("strict_read_csv correctly parses data", {
-#     expected_col_spec <- get_prelim_col_spec()
-#     # get the preliminary greenfeed data schema
-#     parsed_data <- strict_read_csv(
-#         testthat::test_path(
-#             "inst",
-#             "data",
-#             "preliminary-greenfeed-data.csv"
-#         ) # TODO - create a test data file
-#     )
-#     # Define the expected data frame
-#     expected_data <- tibble::tibble(
-#         column1 = c("value1", "value2"),
-#         column2 = c(1.0, 2.0),
-#         column3 = as.Date(c("2021-01-01", "2021-01-02"))
-#     ) # TODO - finish defining the expected data frame
-
-#     # check if the preliminary greenfeed data schema is correct
-#     expect_equal(
-#         parsed_data,
-#         expected_data
-#         )
-# })
-
 test_that("when old csv file and new csv file are equal return the same hash", {
   old_file <- tempfile(fileext = ".csv")
   write.csv(
