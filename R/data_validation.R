f143db2cdda80ddbd235a0b39a286453b0f01a95
amantaya <aantaya@email.arizona.edu>
2025-03-16 18:39:26 -0600

    refactor: change warnings to messages for invalid EID length and prefix in validation functions

--- R/data_validation.R
+++ R/data_validation.R
@@ -18,7 +18,7 @@ validate_eid_length <- function(eid) {
   }
   valid_eid <- (nchar(eid) == 15)
   if (!valid_eid) {
-    warning("EID is not 15 characters in length.")
+    message("EID is not 15 characters in length.")
   }
   valid_eid
 }
@@ -51,7 +51,7 @@ validate_eid_prefix <- function(eid) {
   )
   valid_eid_prefix <- first_three_digits %in% acceptable_prefixes
   if (!valid_eid_prefix) {
-    warning("EID does not have a valid prefix.")
+    message("EID does not have a valid prefix.")
   }
   valid_eid_prefix
 }
