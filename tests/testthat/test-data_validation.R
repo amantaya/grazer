68a4cc1a7ac77e9575ccca1b3e22b72962d4825c
amantaya <aantaya@email.arizona.edu>
2025-03-16 18:54:04 -0600

    test: add sections for tests for improved clarity and organization

--- tests/testthat/test-data_validation.R
+++ tests/testthat/test-data_validation.R
@@ -1,70 +1,58 @@
+# Tests for `validate_eid_length()` -------------------------------
 test_that("eid >15 char in length returns FALSE", {
   expect_equal(validate_eid_length("1234567890123456"), FALSE)
 })
-
 test_that("eid <15 char in length returns FALSE", {
   expect_equal(validate_eid_length("12345678901234"), FALSE)
 })
-
 test_that("eid with missing value returns FALSE", {
   expect_equal(validate_eid_length(NA), FALSE)
 })
-
 test_that("eid ==15 char in length returns TRUE", {
   expect_equal(validate_eid_length("123456789012345"), TRUE)
 })
-
+# Tests for `validate_eid_prefix()` -------------------------------
 test_that("eid with incorrect prefix returns FALSE", {
   expect_equal(validate_eid_prefix("123456789012345"), FALSE)
 })
-
 test_that("eid with correct 982 prefix returns TRUE", {
   expect_equal(validate_eid_prefix("982012345678901"), TRUE)
 })
-
 test_that("eid with correct 985 prefix returns TRUE", {
   expect_equal(validate_eid_prefix("985012345678901"), TRUE)
 })
-
 test_that("eid with correct 840 prefix returns TRUE", {
   expect_equal(validate_eid_prefix("840012345678901"), TRUE)
 })
-
 test_that("eid with correct 942 prefix returns TRUE", {
   expect_equal(validate_eid_prefix("942012345678901"), TRUE)
 })
-
 test_that("eid with correct 949 prefix returns TRUE", {
   expect_equal(validate_eid_prefix("949012345678901"), TRUE)
 })
-
 test_that("eid with missing value returns FALSE", {
   expect_equal(validate_eid_prefix(NA), FALSE)
 })
-
 test_that("eid with incorrect prefix alerts the user", {
   expect_message(
     validate_eid_prefix("123456789012345"),
     "EID does not have a valid prefix."
   )
 })
-
+# Tests for `eid_length()` ----------------------------------------
 test_that("eid with length 15 returns 15", {
   expect_equal(eid_length("123456789012345"), 15)
 })
-
 test_that("eid with length 0 returns 0", {
   expect_equal(eid_length(""), 0)
 })
-
 test_that("eid that is NA returns NA", {
   expect_equal(eid_length(NA), NA_integer_)
 })
-
 test_that("eid as integer of length 15 returns 15", {
   expect_equal(eid_length(123456789012345), 15)
 })
-
+# Tests for `eid_type()` ------------------------------------------
 test_that("eid_type returns correct type for valid prefixes", {
   expect_equal(
     eid_type("840012345678901"),
@@ -75,33 +63,28 @@ test_that("eid_type returns correct type for valid prefixes", {
   expect_equal(eid_type("942012345678901"), "Zee Tags")
   expect_equal(eid_type("949012345678901"), "Y-Tex")
 })
-
 test_that("eid_type returns NA_character_ for invalid prefix", {
   expect_equal(eid_type("123456789012345"), NA_character_)
 })
-
 test_that("eid_type alerts the user for invalid prefix", {
   expect_message(
     eid_type("123456789012345"),
     "EID does not have a valid prefix."
   )
 })
-
 test_that("eid_type alerts the user for invalid eid length", {
   expect_message(
     eid_type("1234567890123456"),
     "EID does not have a valid prefix."
   )
 })
-
 test_that("eid_type returns NA_character_ for NA input", {
   expect_equal(eid_type(NA), NA_character_)
 })
-
 test_that("eid_type returns NA_character_ for empty string", {
   expect_equal(eid_type(""), NA_character_)
 })
-
+# Tests for `eid_prefix()` ----------------------------------------
 test_that("eid_prefix returns correct prefix for valid character EID", {
   expect_equal(eid_prefix("982012345678901"), "982")
   expect_equal(eid_prefix("985012345678901"), "985")
@@ -109,8 +92,6 @@ test_that("eid_prefix returns correct prefix for valid character EID", {
   expect_equal(eid_prefix("942012345678901"), "942")
   expect_equal(eid_prefix("949012345678901"), "949")
 })
-
-
 test_that("eid_prefix returns correct prefix for valid integer EID", {
   expect_equal(eid_prefix(982012345678901), "982")
   expect_equal(eid_prefix(985012345678901), "985")
@@ -118,17 +99,14 @@ test_that("eid_prefix returns correct prefix for valid integer EID", {
   expect_equal(eid_prefix(942012345678901), "942")
   expect_equal(eid_prefix(949012345678901), "949")
 })
-
 test_that("eid_prefix returns NA_character_ for NA input", {
   expect_equal(eid_prefix(NA), NA_character_)
 })
-
 test_that("eid_prefix returns correct prefix for short EID", {
   expect_equal(eid_prefix("123"), "123")
   expect_equal(eid_prefix("12"), "12")
   expect_equal(eid_prefix("1"), "1")
 })
-
 test_that("eid_prefix returns empty string for empty input", {
   expect_equal(eid_prefix(""), "")
 })
